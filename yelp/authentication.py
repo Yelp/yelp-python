27abe25c326a8e0d348cfd99ad65be452c2cc18a
Dinah Shi <dinahshi28@gmail.com>
2015-10-05 14:29:56 -0700

    Move request logic to authentication class

--- yelp/authentication.py
+++ yelp/authentication.py
@@ -1,5 +1,7 @@
 # -*- coding: UTF-8 -*-
+import json
 import oauth2
+import urllib2
 
 
 class Authentication():
@@ -11,3 +13,30 @@ class Authentication():
 
         self.consumer = oauth2.Consumer(self.consumer_key, self.consumer_secret)
         self.oauth_token = oauth2.Token(self.token, self.token_secret)
+
+    def request(self, url, url_params={}):
+        oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
+        oauth_request.update(
+            {
+                'oauth_nonce': oauth2.generate_nonce(),
+                'oauth_timestamp': oauth2.generate_timestamp(),
+                'oauth_token': self.token,
+                'oauth_consumer_key': self.consumer_key
+            }
+        )
+        oauth_request.sign_request(
+            oauth2.SignatureMethod_HMAC_SHA1(),
+            self.consumer,
+            self.oauth_token
+        )
+        signed_url = oauth_request.to_url()
+
+        print u'Querying {0} ...'.format(url)
+
+        conn = urllib2.urlopen(signed_url, None)
+        try:
+            response = json.loads(conn.read())
+        finally:
+            conn.close()
+
+        return response
--- yelp/client.py
+++ yelp/client.py
@@ -1,7 +1,5 @@
 # -*- coding: UTF-8 -*-
-import json
 import urllib
-import urllib2
 
 from yelp.config import API_HOST
 from yelp.config import SEARCH_PATH
@@ -15,32 +13,7 @@ class Client():
 
     def request(self, path, url_params={}):
         url = 'http://{0}{1}?'.format(API_HOST, urllib.quote(path))
-
-        oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
-        oauth_request.update(
-            {
-                'oauth_nonce': oauth2.generate_nonce(),
-                'oauth_timestamp': oauth2.generate_timestamp(),
-                'oauth_token': self.authentication.token,
-                'oauth_consumer_key': self.authentication.consumer_key
-            }
-        )
-        oauth_request.sign_request(
-            oauth2.SignatureMethod_HMAC_SHA1(),
-            self.authentication.consumer,
-            self.authentication.oauth_token
-        )
-        signed_url = oauth_request.to_url()
-        
-        print u'Querying {0} ...'.format(url)
-
-        conn = urllib2.urlopen(signed_url, None)
-        try:
-            response = json.loads(conn.read())
-        finally:
-            conn.close()
-
-        return response
+        return self.authentication.request(url, url_params)
 
     def get_business(self, business_id):
         business_path = BUSINESS_PATH + business_id

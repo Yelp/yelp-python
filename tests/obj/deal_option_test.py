b33b2f8a1e32f8e2ed5be7302d8036d82d72d857
Dinah Shi <dinahshi28@gmail.com>
2015-11-09 12:08:54 -0800

    Rename init tests for DealOption and GiftCertificateOption
    Closes #12

--- tests/obj/deal_option_test.py
+++ tests/obj/deal_option_test.py
@@ -4,7 +4,7 @@ import json
 from yelp.obj.deal_option import DealOption
 
 
-def test_init_deal():
+def test_init_deal_option():
     with open('json/business_response.json') as biz:
         response = json.load(biz)['deals'][0]['options'][0]
         deal_option = DealOption(response)
--- tests/obj/gift_certificate_option_test.py
+++ tests/obj/gift_certificate_option_test.py
@@ -4,7 +4,7 @@ import json
 from yelp.obj.gift_certificate_option import GiftCertificateOption
 
 
-def test_init_deal():
+def test_init_gift_certificate_option():
     with open('json/business_response.json') as biz:
         response = json.load(biz)['gift_certificates'][0]['options'][0]
         gift_certificate_option = GiftCertificateOption(response)

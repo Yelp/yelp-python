d8ac06384b322e46b93c3e2d840d6f32c6072f7a
Dinah Shi <dinahshi28@gmail.com>
2015-11-11 13:04:44 -0800

    Prep for publishing to PyPI

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name='yelp',
+
+    version='1.0',
+
+    description='Python Clientlib for Yelp Public API',
+
+    url='https://github.com/Yelp/yelp-python',
+
+    author='Yelp',
+    author_email='partnerships@yelp.com',
+
+    license='MIT',
+
+    keywords='yelp',
+
+    packages=[
+        'yelp',
+        'yelp.obj'
+    ],
+
+    install_requires=[
+        'httplib2==0.9.2',
+        'oauth2==1.9.0.post1'
+    ],
+
+    download_url='https://github.com/Yelp/yelp-python/tarball/1.0'
+)
